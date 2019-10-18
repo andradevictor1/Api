d4e608d668994935e3c55c532dc7c17ae3d93dbf
andradevictor1 <victorandrade1994@gmail.com>
2019-10-18 20:18:57 -0300

    Projeto Api

---
+++ src/main/java/br/com/unipac/cliente/api/clienteapi/ClienteApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.unipac.cliente.api.clienteapi;
+
+import org.springframework.boot.SpringApplication;
+
+
+
+public class ClienteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/unipac/cliente/api/clienteapi/ClienteApiApplicationTests.java
@@ -0,0 +1,9 @@
+package br.com.unipac.cliente.api.clienteapi;
+
+
+
+
+class ClienteApiApplicationTests {
+
+
+}
